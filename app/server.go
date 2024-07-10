cad4c1799cac49f780a27395d2c69de7d4d0b9c2
Feras Muki <feras.muki@gmail.com>
2024-07-09 23:59:15 -0400

    Implement user-agent GET endpoint to read the user-agent header

--- app/server.go
+++ app/server.go
@@ -38,15 +38,16 @@ func main() {
 			os.Exit(1)
 		}
 		buf = append(buf, tmp[:n]...)
-		crlf_index := bytes.Index(buf, []byte("\r\n"))
-		if crlf_index != -1 {
-			buf = buf[:crlf_index]
+		dcrlf_index := bytes.Index(buf, []byte("\r\n\r\n"))
+		if dcrlf_index != -1 {
+			buf = buf[:dcrlf_index]
 			break
 		}
 	}
 
-	request_line := strings.Split(string(buf), " ")
-	request_target := request_line[1]
+	request_line := strings.Split(string(buf), "\r\n")[0]
+	headers := strings.Split(string(buf), "\r\n")[1:]
+	request_target := strings.Split(request_line, " ")[1]
 
 	if matched, _ := regexp.MatchString(`^/$`, request_target); matched {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
@@ -57,6 +58,20 @@ func main() {
 		message := r.FindStringSubmatch(request_target)[1]
 
 		fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)
+	} else if matched, _ := regexp.MatchString(`^/user-agent$`, request_target); matched {
+		user_agent := ""
+		for _, header := range headers {
+			split := strings.Split(header, ": ")
+			name := split[0]
+			value := split[1]
+
+			if name == "User-Agent" {
+				user_agent = value
+				break
+			}
+		}
+
+		fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(user_agent), user_agent)
 	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
