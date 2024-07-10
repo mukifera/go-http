1e5576cc6caf98a2cc30b39617096073cd889f37
Feras Muki <feras.muki@gmail.com>
2024-07-09 22:15:24 -0400

    Implements 404 responses for unknown GET requests

--- app/server.go
+++ app/server.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"io"
+	"bytes"
+	"strings"
 )
 
 func main() {
@@ -19,5 +22,36 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	// defer conn.Close()
+
+	buf := make([]byte, 0, 4096)
+	tmp := make([]byte, 256)
+
+	for {
+		n, err := conn.Read(tmp)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("Read error:", err)
+			os.Exit(1)
+		}
+		buf = append(buf, tmp[:n]...)
+		crlf_index := bytes.Index(buf, []byte("\r\n"))
+		if crlf_index != -1 {
+			buf = buf[:crlf_index]
+			break
+		}
+	}
+
+	request_line := strings.Split(string(buf), " ")
+
+	if len(request_line) == 3 && request_line[1] != "/" {
+		fmt.Println(request_line[1])
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	}
+
+	
 }
