9895362b7d9669b5908abb72a2fce43e985f0b5d
Feras Muki <feras.muki@gmail.com>
2024-07-09 23:22:36 -0400

    Implement the echo endpoint

--- app/server.go
+++ app/server.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"bytes"
 	"strings"
+	"regexp"
 )
 
 func main() {
@@ -22,7 +23,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	// defer conn.Close()
+	defer conn.Close()
 
 	buf := make([]byte, 0, 4096)
 	tmp := make([]byte, 256)
@@ -45,13 +46,19 @@ func main() {
 	}
 
 	request_line := strings.Split(string(buf), " ")
+	request_target := request_line[1]
 
-	if len(request_line) == 3 && request_line[1] != "/" {
-		fmt.Println(request_line[1])
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-	} else {
+	if matched, _ := regexp.MatchString(`^/$`, request_target); matched {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	}
+	} else if matched, _ := regexp.MatchString(`^/echo(/\w*)*$`, request_target); matched {
 
+		r := regexp.MustCompile(`^/echo(?:/(\w*))*$`)
+
+		message := r.FindStringSubmatch(request_target)[1]
+
+		fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(message), message)
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
 	
 }
